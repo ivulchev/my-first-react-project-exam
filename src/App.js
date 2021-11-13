2d1b7b4407a39c7c957ea75228c4a740be808002
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-13 12:08:09 +0200

    created Footer component and Footer CSS module
    Component is imported in APP and visualize.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />,
-      <Home />
+      <Home />,
+      <Footer/>
     </div>
   );
 }
