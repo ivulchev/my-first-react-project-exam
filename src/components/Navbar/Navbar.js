be52b808aebafb257ad3c8b455549ba2721da73c
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-13 16:48:42 +0200

    changed name of the website now is F1 FanHome

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import  "./Navbar.module.css"
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light ">
-            <a className="navbar-brand" href="/"><mark>F1 Madness</mark></a>
+            <a className="navbar-brand" href="/"><mark>F1 FanHome</mark></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
