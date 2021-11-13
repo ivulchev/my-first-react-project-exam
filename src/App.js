90669e760842f15c62dd645c2c2aa348b7a756ab
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-13 13:51:18 +0200

    Pilots page with cards
    Created Pilots Page with partial Card and  CSS module

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import Home from './components/Home/Home';
+//import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import Login from './components/Login/Login';
-import Register from "./components/Register/Register"
+//import Login from './components/Login/Login';
+//import Register from "./components/Register/Register";
+import Pilots from './components/Pilots/Pilots';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />,
-      <Login />,
-      <Register />,
+      <Pilots />,
       <Footer/>
     </div>
   );
---
+++ src/components/Pilots/Pilots.js
@@ -0,0 +1,13 @@
+import Card from "./Card"
+
+function Pilots() {
+    return (
+        <header>
+            <div className="row">
+                <Card img="https://www.formula1.com/content/fom-website/en/drivers/max-verstappen/_jcr_content/image.img.640.medium.jpg/1617101447981.jpg" name="Max Verstappen" description="Born 30 September 1997. Verstappen is a Belgian-Dutch racing driver currently competing in Formula One, under the Dutch flag, with Red Bull Racing. At the 2015 Australian Grand Prix, when he was aged 17 years, 166 days, he became the youngest driver to compete in Formula One. "/>, 
+            </div>
+        </header>
+    )
+}
+
+export default Pilots
