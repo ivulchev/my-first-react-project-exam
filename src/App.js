7e65ffa808fee8217dc520813b592cc25cac86d3
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-13 11:43:07 +0200

    created background and Home page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+        <header id="header">
+            <p id="home-paragraph">"First, you have to finish." – Michael Schumacher</p>
+        </header>
+    )
+}
+export default Home
