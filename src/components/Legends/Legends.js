f4642c1990eb5cc8c0f34c23253d3b9d527c8c39
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-13 16:58:35 +0200

    created Legends component with CSS module

---
+++ src/components/Legends/Legends.js
@@ -0,0 +1,13 @@
+import LegendsCard from "./LegendsCard"
+
+function Legends() {
+    return (
+        <header>
+            <div className="row">
+                <LegendsCard img="https://www.sportfair.it/wp-content/uploads/2019/01/Michael-Schumacher-3.jpg" name="Michael Schumacher" description="Born 3 January 1969. Michael is a German former racing driver who competed in Formula One for Jordan, Benetton, Ferrari, and Mercedes. Schumacher has a joint-record seven World Drivers' Championship titles (tied with Lewis Hamilton) and, at the time of his retirement from the sport in 2012, he held the records for the most wins (91), pole positions (68) and podium finishes (155)—which have since been broken by Hamilton—while he maintains the record for the most fastest laps (77), amongst others."/>, 
+            </div>
+        </header>
+    )
+}
+
+export default Legends
---
+++ src/components/Legends/LegendsCard.js
@@ -0,0 +1,23 @@
+import styles from "./LegendsCard.module.css"
+function LegendsCard(props) {
+    return (
+        <div className="card" id={styles.cardPartial}>
+            <img src={props.img} alt="Card img cap" />
+            <div className="card-body">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text" id={styles.description} >{props.description}</p>
+                <p className="rating">Rating: </p>
+                <button href="/details"className={styles.detailsBtn} >
+                    Details
+                </button>
+                <button className={styles.upBtn} >
+                    Up
+                </button>
+                <button className={styles.downBtn} >
+                    Down
+                </button>
+            </div>
+        </div>
+    )
+}
+export default LegendsCard
