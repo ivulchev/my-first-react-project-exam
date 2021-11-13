1dcc12ce4e7b03ec64667c1df1cf7f2de4182f30
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-13 14:24:17 +0200

    created Details component
    created Details comp and CSS module.
    Updated Footer.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 //import Login from './components/Login/Login';
 //import Register from "./components/Register/Register";
-import Pilots from './components/Pilots/Pilots';
+//import Pilots from './components/Pilots/Pilots';
+import PilotDetails from './components/PilotDetails/PilotDetails';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />,
-      <Pilots />,
+      <PilotDetails img="https://www.formula1.com/content/fom-website/en/drivers/max-verstappen/_jcr_content/image.img.640.medium.jpg/1617101447981.jpg" 
+      name = "Max Verstappen"
+      description="Born 30 September 1997. Verstappen is a Belgian-Dutch racing driver currently competing in Formula One, under the Dutch flag, with Red Bull Racing. At the 2015 Australian Grand Prix, when he was aged 17 years, 166 days, he became the youngest driver to compete in Formula One. " />,
       <Footer/>
     </div>
   );
---
+++ src/components/PilotDetails/PilotDetails.js
@@ -0,0 +1,17 @@
+import styles from "./PilotDetails.module.css"
+function pilotDetails(props) {
+    return (
+        <div className="card" id={styles.details} >
+            <img src={props.img} className="card-img-top" alt="..."/>
+            <div className ="card-body">
+            <h5 className ="card-title">{props.name}</h5>
+            <p className ="card-text">{props.description}</p>
+            </div>
+            
+            <div className ="card-body">
+            <a href="/pilots" className ="card-link">Go Back</a>
+            </div>
+        </div>
+    )
+}
+export default pilotDetails
