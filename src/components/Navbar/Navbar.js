c18f22a64937dfc698ad3a6de759b3da26a6adbd
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-17 21:23:18 +0200

    Added NavLink to navigation
    added navlink to navigation with activeclassname who change background color and hover with color

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,43 @@
-import  "./Navbar.module.css"
-import { Link} from "react-router-dom";
+import  "./Navbar.css"
+import { NavLink} from "react-router-dom";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light ">
-            <Link className="navbar-brand" to="/"><mark>F1 FanHome</mark></Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <NavLink className="navbar-brand" activeClassName="active" to="/"><mark>F1 FanHome</mark></NavLink>
+            <button className="navbar-toggler" type="button" data-toggle ="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/pilots"><strong><mark>Pilots</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/pilots"><strong><mark>Pilots</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/teams"><strong><mark>Teams</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/teams"><strong><mark>Teams</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/legends"><strong><mark>Legends</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/legends"><strong><mark>Legends</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/posts/my-posts"><strong><mark>My Posts</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/posts/my-posts"><strong><mark>My Posts</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/posts/create"><strong><mark>Create Post</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/posts/create"><strong><mark>Create Post</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/posts/all"><strong><mark>All Posts</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/posts/all"><strong><mark>All Posts</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/calendar"><strong><mark>Calendar</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/calendar"><strong><mark>Calendar</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/login"><strong><mark>Login</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/login"><strong><mark>Login</mark></strong></NavLink>
+                    </li>
+                    <li className="nav-item" >
+                        <NavLink className="nav-link" activeClassName="active" to="/logout"><strong><mark>Logout</mark></strong></NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/logout"><strong><mark>Logout</mark></strong></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/register"><strong><mark>Register</mark></strong></Link>
+                        <NavLink className="nav-link" activeClassName="active" to="/register"><strong><mark>Register</mark></strong></NavLink>
                     </li>
                 </ul>
             </div>
