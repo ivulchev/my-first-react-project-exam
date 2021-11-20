e16b2886602a0922d93f2a5f347089c9a295397c
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-20 19:32:18 +0200

    Updated Error Page with Alert

--- src/components/Error/ErrorPage.js
+++ src/components/Error/ErrorPage.js
@@ -1,5 +1,6 @@
 import styles from "./Error.module.css"
 function Error(){
+    alert("404 Page not found!")
     return(
         <div>
         <img src="https://wallpaperaccess.com/full/6725718.jpg" className={styles.imgFluid} alt="..."></img>
