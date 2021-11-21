ab66b2fe414dc5890bddec00f9cadb923a48692c
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-21 11:38:51 +0200

    updated Legends Component
    fetching and rendering from test server

--- src/components/Legends/Legends.js
+++ src/components/Legends/Legends.js
@@ -1,10 +1,18 @@
 import LegendsCard from "./LegendsCard"
-
+import { useEffect, useState } from "react";
 function Legends() {
+    const [legends, setLegends] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:3030/data/legends`)
+            .then(res => res.json())
+            .then(result => {
+                setLegends(result)
+            });
+    },[]);
     return (
         <header>
             <div className="row">
-                <LegendsCard img="https://www.sportfair.it/wp-content/uploads/2019/01/Michael-Schumacher-3.jpg" name="Michael Schumacher" description="Born 3 January 1969. Michael is a German former racing driver who competed in Formula One for Jordan, Benetton, Ferrari, and Mercedes. Schumacher has a joint-record seven World Drivers' Championship titles (tied with Lewis Hamilton) and, at the time of his retirement from the sport in 2012, he held the records for the most wins (91), pole positions (68) and podium finishes (155)—which have since been broken by Hamilton—while he maintains the record for the most fastest laps (77), amongst others."/>, 
+               {legends.map(x =><LegendsCard key={x._id} legend={x}/> )}, 
             </div>
         </header>
     )
--- src/components/Legends/LegendsCard.js
+++ src/components/Legends/LegendsCard.js
@@ -1,11 +1,11 @@
 import styles from "./LegendsCard.module.css"
-function LegendsCard(props) {
+function LegendsCard({legend}) {
     return (
         <div className="card" id={styles.cardPartial}>
-            <img src={props.img} alt="Card img cap" />
+            <img src={legend.logoUrl} alt="Card img cap" />
             <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <p className="card-text" id={styles.description} >{props.description}</p>
+                <h5 className="card-title">{legend.name}</h5>
+                <p className="card-text" id={styles.description} >{legend.description}</p>
                 <p className="rating">Rating: </p>
                 <button href="/details"className={styles.detailsBtn} >
                     Details
