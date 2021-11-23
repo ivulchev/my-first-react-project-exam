3db25a7798042f191fd9ef0fbbcc9c2a780e1959
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-23 22:23:40 +0200

    updated Navbar with isAuthenticated
    show login and register if is not logged
    and show logout when its login
    must be fixed re rendering navigation after login and logout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,17 @@
-import  "./Navbar.css"
-import { NavLink} from "react-router-dom";
+import "./Navbar.css"
+import { NavLink } from "react-router-dom";
+import { authServices } from "../../services/authService";
 function Navbar() {
     return (
         <nav id="navbar" className="navbar navbar-expand-lg navbar-light bg-light">
             <NavLink className="navbar-brand" activeClassName="active" to="/">F1 FanHome</NavLink>
-            <button className="navbar-toggler" type="button" data-toggle ="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/pilots">Pilots</NavLink>
+                        <NavLink className="nav-link" activeClassName="active" to="/pilots">Drivers</NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" activeClassName="active" to="/teams">Teams</NavLink>
@@ -19,26 +20,32 @@ function Navbar() {
                         <NavLink className="nav-link" activeClassName="active" to="/legends">Legends</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/posts/my-posts">My Posts</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/posts/create">Create Post</NavLink>
-                    </li>
-                    <li className="nav-item">
                         <NavLink className="nav-link" activeClassName="active" to="/posts/all">All Posts</NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" activeClassName="active" to="/calendar">Calendar</NavLink>
                     </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
-                    </li>
-                    <li className="nav-item" >
-                        <NavLink className="nav-link" activeClassName="active" to="/logout">Logout</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/register">Register</NavLink>
-                    </li>
+                    {authServices.isAuthenticated ?
+                        (<ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/posts/my-posts">My Posts</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/posts/create">Create Post</NavLink>
+                            </li>
+
+                            <li className="nav-item" >
+                                <NavLink className="nav-link" activeClassName="active" to="/logout">Logout</NavLink>
+                            </li>
+                        </ul>) :
+                        (<ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/register">Register</NavLink>
+                            </li>
+                        </ul>)}
                 </ul>
             </div>
         </nav>
