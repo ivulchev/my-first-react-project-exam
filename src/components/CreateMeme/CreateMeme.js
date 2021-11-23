c3734e4bc8b332f0568c811e79ceb94c6be37ef3
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-23 20:35:27 +0200

    replaced for tag in forms with htmlFor

--- src/components/CreateMeme/CreateMeme.js
+++ src/components/CreateMeme/CreateMeme.js
@@ -4,11 +4,11 @@ function CreateMeme() {
         <form  id={styles.createMeme}>
             <h1 id={styles.pageTitle}>Create MEME</h1>
             <div className="form-group">
-                <label for="titleMEME" id={styles.pageTitle}>Title</label>
+                <label htmlFor="titleMEME" id={styles.pageTitle}>Title</label>
                 <input type="email" className="form-control" id="titleMEME" aria-describedby="emailHelp" placeholder="Add Title"/>
             </div>
             <div className="form-group">
-                <label for="imageMEME" id={styles.pageTitle}>Image URL</label>
+                <label htmlFor="imageMEME" id={styles.pageTitle}>Image URL</label>
                 <input type="text" className="form-control" id="imageMEME" placeholder="Add image URL "/>
             </div>
             <button type="submit" id={styles.createBtn}>Submit</button>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,11 +20,11 @@ function Login() {
         <form onSubmit={onSubmit} className={styles.loginForm}>
             <h1 id={styles.pageTitle}>Login</h1>
             <div className="form-group">
-                <label for="exampleInputEmail1" id={styles.pageTitle}>Email address</label>
+                <label htmlFor="exampleInputEmail1" id={styles.pageTitle}>Email address</label>
                 <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
             </div>
             <div className="form-group">
-                <label for="exampleInputPassword1" id={styles.pageTitle}>Password</label>
+                <label htmlFor="exampleInputPassword1" id={styles.pageTitle}>Password</label>
                 <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
             </div>
             <button type="submit" className={styles.loginBtn}>Submit</button>
