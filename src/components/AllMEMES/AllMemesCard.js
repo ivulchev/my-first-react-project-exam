bff94cfd0ecb6996da07f83fc24b38fdd016b515
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-11-29 18:08:39 +0200

    updated all memes
    vote buttons will be visible only for login users other user will see link to login page

--- src/components/AllMEMES/AllMemesCard.js
+++ src/components/AllMEMES/AllMemesCard.js
@@ -1,17 +1,22 @@
 import styles from "./AllMEMES.module.css"
+import { Link } from "react-router-dom"
 function allMemesCard({meme}){
+    let upButton = <button className={styles.upBtn} >
+        Up
+    </button>
+    let downButton = <button className={styles.downBtn} >
+        Down
+    </button>
+    let buttons = [upButton, downButton]
     return (
         <div className="card" id={styles.cardPartial}>
             <img src={meme.image} id={styles.img} alt="Card img cap" />
             <div className="card-body">
                 <h5 className="card-title">{meme.title}</h5>
                 <p className="rating">Rating: {meme.rating} </p>
-                <button className={styles.upBtn} >
-                    Up
-                </button>
-                <button className={styles.downBtn} >
-                    Down
-                </button>
+                {localStorage.email ?
+                buttons :
+                <Link to="/login">  Please, login to vote!</Link>}
             </div>
         </div>
     )
