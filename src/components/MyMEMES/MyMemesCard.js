00dd78ae4b7a1284e7d4d575a7f07423078454b7
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-12-04 22:32:09 +0200

    updated Mymemes with delete functionality and redirecting to create

--- src/components/MyMEMES/MyMemesCard.js
+++ src/components/MyMEMES/MyMemesCard.js
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./MyMEMES.module.css";
+import * as requester from "../../services/requester";
+import { useHistory } from "react-router";
 function MyMemesCard({meme}){
+    let history = useHistory()
+    function deleteMeme(){
+        requester.del(`http://localhost:3030/jsonstore/memes/${meme._id}`)
+        console.log("deleted")
+            return history.push("/posts/create")
+
+    }
     return (
         <div className="card" id={styles.cardPartial}>
             <img src={meme.image} id={styles.img} alt="Card img cap" />
@@ -11,7 +20,7 @@ function MyMemesCard({meme}){
                     Edit
                 </button>
                 </Link>
-                <button className={styles.downBtn} >
+                <button className={styles.downBtn} onClick={deleteMeme}>
                     Delete
                 </button>
             </div>
