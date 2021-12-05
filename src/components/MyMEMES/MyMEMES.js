c415332d3f7bd1319c356b5d36546442a5a4025d
ivulchev <81465925+ivulchev@users.noreply.github.com>
2021-12-05 16:45:57 +0200

    Update MyMEMES.js

--- src/components/MyMEMES/MyMEMES.js
+++ src/components/MyMEMES/MyMEMES.js
@@ -1,5 +1,6 @@
 import MyMemesCard from "./MyMemesCard"
 import { useEffect, useState } from "react";
+import ErrorPage from "../Error/ErrorPage";
 function MyMEMES() {
     const [myMemes, setMyMemes] = useState([]);
     useEffect(() => {
@@ -11,7 +12,7 @@ function MyMEMES() {
                 setMyMemes(array)
             });
     },[]);
-    return (
+    return ( myMemes === undefined ? <ErrorPage/>:
         <div className="row">
             {myMemes.map((x) => <MyMemesCard key={x._id} meme={x}/>)}
         </div>
