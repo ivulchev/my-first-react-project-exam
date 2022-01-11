a7db8bf07e33efa926765b04cb6a14aa4bb44409
ivulchev <81465925+ivulchev@users.noreply.github.com>
2022-01-11 18:31:06 +0200

    added modal dialog box to meme voting

--- src/components/AllMEMES/AllMemesCard.js
+++ src/components/AllMEMES/AllMemesCard.js
@@ -4,6 +4,7 @@ import * as requester from "../../services/requester";
 import { useState, useEffect, useContext } from "react";
 import { endpoints } from "../../services/services";
 import { AuthContext } from "../../contexts/AuthContext";
+import { Modal, Button } from "react-bootstrap";
 
 
 function AllMemesCard({ meme }) {
@@ -11,11 +12,25 @@ function AllMemesCard({ meme }) {
     const [memeInfo, setMemeInfo] = useState(meme[1])
     const { user } = useContext(AuthContext);
     const [rating, setRating] = useState(meme[1].rating);
+    const [show, setShow] = useState(false);
+    const [upDown, setUpDown] = useState("");
+    function clickUp(){
+        handleShow()
+        setUpDown("Up")
+    }
+    function clickDown(){
+        handleShow()
+        setUpDown("Down")
+    }
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     function voteUp(e) {
         e.preventDefault();
         requester.patch(`${endpoints.baseUrl}memes/${id}.json`, { rating: rating + 1, voters: [...memeInfo.voters, user] })
         .then(() => {
             setRating(rating + 1);
+            handleClose();
         })
     }
     function voteDown(e) {
@@ -23,6 +38,7 @@ function AllMemesCard({ meme }) {
         requester.patch(`${endpoints.baseUrl}memes/${id}.json`, { rating: rating - 1, voters: [...memeInfo.voters, user] })
         .then(() => {
             setRating(rating - 1);
+            handleClose();
         })
     }
     const [isOwner, setIsOwner] = useState()
@@ -43,10 +59,10 @@ function AllMemesCard({ meme }) {
     }, [rating])
 
 
-    let upButton = <button className={styles.upBtn} onClick={voteUp}>
+    let upButton = <button className={styles.upBtn} onClick={clickUp}>
         Up
     </button>
-    let downButton = <button className={styles.downBtn} onClick={voteDown}>
+    let downButton = <button className={styles.downBtn} onClick={clickDown}>
         Down
     </button>
     let buttons = [upButton, downButton]
@@ -73,6 +89,20 @@ function AllMemesCard({ meme }) {
                     <Voted /> :
                     <Link to="/login" id={styles.loginLink}>  Please, login to vote!</Link>}
             </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>F1 FanHome:</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>You can vote only once per post! Do you really want to proceed?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={upDown === "Up" ? voteUp : voteDown}>
+                        Yes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div> 
     )
 }
