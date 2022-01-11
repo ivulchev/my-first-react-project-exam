69e37e3bae58ece7e16395c1b72c3d37f67575f5
ivulchev <81465925+ivulchev@users.noreply.github.com>
2022-01-11 18:42:34 +0200

    added modal dialog box to delete meme

--- src/components/MyMEMES/MyMemesCard.js
+++ src/components/MyMEMES/MyMemesCard.js
@@ -4,19 +4,30 @@ import * as requester from "../../services/requester";
 import { useHistory } from "react-router";
 import { endpoints } from "../../services/services";
 import { useState } from "react";
-import {useNotificationContext, types} from "../../contexts/NotificationContext"
+import { useNotificationContext, types } from "../../contexts/NotificationContext";
+import { Modal, Button } from "react-bootstrap";
 function MyMemesCard({ meme }) {
     const [id, setId] = useState(meme[0]);
     const [memeInfo, setMemeInfo] = useState(meme[1]);
-    const {addNotification} = useNotificationContext();
+    const { addNotification } = useNotificationContext();
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     let history = useHistory()
     function deleteMeme() {
-        if(window.confirm("Do you really want to delete this item?")){
         requester.del(`${endpoints.baseUrl}memes/${id}.json`)
-        addNotification("You succesfully delete you posts!", types.succes)
-        history.push("/posts/create")
-        }
-
+            .then(() => {
+                addNotification("You succesfully delete you posts!", types.succes)
+                handleClose()
+                history.push("/posts/create")
+            })
+            .catch((function (error) {
+                let errorMessage = error.message;
+                handleClose()
+                addNotification(errorMessage, types.error)
+            }));
     }
     return (
         <div className="card" id={styles.cardPartial}>
@@ -28,10 +39,24 @@ function MyMemesCard({ meme }) {
                     Edit
                 </button>
                 </Link>
-                <button className={styles.downBtn} onClick={deleteMeme}>
+                <button className={styles.downBtn} onClick={handleShow}>
                     Delete
                 </button>
             </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>F1 FanHome:</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Do you really want to delete your post?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={deleteMeme}>
+                        Yes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
